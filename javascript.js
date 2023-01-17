1e95476eef389450c54d57cd2e2598e09eada05d
oaktec <54672803+oaktec@users.noreply.github.com>
2023-01-17 14:20:14 +0000

    Implement core calculator functionality

--- javascript.js
+++ javascript.js
@@ -5,10 +5,14 @@ const KEYPAD_MAP = [[" ", " ", " ", "C"],
                     ["4", "5", "6", "×"], 
                     ["1", "2", "3", "−"], 
                     ["0", ".", "=", "+"]];
+const OPERATORS = ["÷", "+", "×", "−"];
 
 const keypadContainer = document.querySelector(".keypad-container");
 const outputContainer = document.querySelector(".output-container")
-let displayValue = "";
+let displayValue = "0";
+let termOne;
+let operator;
+let termTwo;
 generateCalculator();
 
 function generateCalculator() {
@@ -27,13 +31,82 @@ function generateCalculator() {
 }
 function determineKey(x, y, key) {
     key.textContent = KEYPAD_MAP[y][x];
-    key.addEventListener("click", (e) => keyCallback(key.textContent));
+    if (key.textContent !== " ") {
+        key.addEventListener("click", (e) => keyCallback(key.textContent));
+    }
     return key;
 }
 
 function keyCallback (keyText) {
-    displayValue += keyText;
+    switch (true) {
+        case OPERATORS.includes(keyText):
+            if (operator)  { // already picked an operator
+                if (operatorLastLine()) { //changing current operator
+                    setOperator(keyText);
+                    displayValue = displayValue.slice(0, -1) + keyText;
+                } else { // evaluating equation and setting new
+                    evalEquation(keyText);
+                }
+                
+            } else {
+                termOne = parseFloat(displayValue);
+                setOperator(keyText);
+                displayValue += keyText;
+            }
+
+            if (operatorLastLine()) {//just picked an operator
+                displayValue = displayValue.slice(0, -1) + keyText;
+            } else {
+                displayValue += keyText;
+            }
+            break;
+        case keyText === "C":
+            displayValue = "0";
+            break;
+        case keyText === "=":
+            if (operator && !operatorLastLine())
+                evalEquation();
+            break;
+        default:
+            if (displayValue === "0" && keyText !== ".") {
+                displayValue = "";
+            }
+            displayValue += keyText;
+            break;
+    }
     outputContainer.textContent = displayValue;
+}
+function evalEquation(nextOperator) {
+    termTwo = parseFloat(displayValue.split(/[÷+×−]/)[1]);
+    displayValue = "" + operate(operator, termOne, termTwo);
+    if (nextOperator) {
+        termOne = parseFloat(displayValue);
+        setOperator(nextOperator);
+        displayValue += nextOperator;
+    } else {
+        operator = undefined;
+        termOne = undefined;
+    }
+    termTwo = undefined;
+}
+function operatorLastLine() {
+    return OPERATORS.includes(displayValue.slice(-1));
+}
+function setOperator(keyText) {
+    switch (keyText) {
+        case "÷":
+            operator = "/";
+            break;
+        case "+":
+            operator = "+";
+            break;
+        case "×":
+            operator = "*";
+            break;
+        case "−":
+            operator = "-";
+            break;
+    }
 }
 function add(a,b) {
     return a + b;
