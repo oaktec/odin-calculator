e29bca7405edc7c681b3f6063b33c5797b9a82bd
oaktec <54672803+oaktec@users.noreply.github.com>
2023-01-17 12:22:11 +0000

    Generate key text from grid

--- javascript.js
+++ javascript.js
@@ -13,9 +13,79 @@ function generateCalculator() {
         for (let y = 0; y < KEYPAD_NUMBER_OF_ROWS; y++) {
             let key = document.createElement("div");
             key.classList.add("key");
+            key = determineKey(x, y, key);
             keypadCol.append(key);
         }
     }
+}
+function determineKey(x, y, key) {
+    switch(x) {
+        case 0:
+            switch (y) {
+                case 0:
+                    key.textContent = "7";
+                    break;
+                case 1:
+                    key.textContent = "4";
+                    break;
+                case 2:
+                    key.textContent = "1";
+                    break;
+                case 3:
+                    key.textContent = "0";
+                    break;
+            }
+            break;
+        case 1:
+            switch (y) {
+                case 0:
+                    key.textContent = "8";
+                    break;
+                case 1:
+                    key.textContent = "5";
+                    break;
+                case 2:
+                    key.textContent = "2";
+                    break;
+                case 3:
+                    key.textContent = ".";
+                    break;
+            }
+            break;
+        case 2:
+            switch (y) {
+                case 0:
+                    key.textContent = "9";
+                    break;
+                case 1:
+                    key.textContent = "6";
+                    break;
+                case 2:
+                    key.textContent = "3";
+                    break;
+                case 3:
+                    key.textContent = "=";
+                    break;
+            }
+            break;
+        case 3:
+            switch (y) {
+                case 0:
+                    key.textContent = "/";
+                    break;
+                case 1:
+                    key.textContent = "X";
+                    break;
+                case 2:
+                    key.textContent = "-";
+                    break;
+                case 3:
+                    key.textContent = "+";
+                    break;
+            }
+            break;
+    }
+    return key;
 }
 
 
