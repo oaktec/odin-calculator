6f5701f124a9d4add617fd55d5d5725003d812d0
oaktec <54672803+oaktec@users.noreply.github.com>
2023-01-17 12:01:13 +0000

    Outline basic calculator ui

--- javascript.js
+++ javascript.js
@@ -1,3 +1,24 @@
+const KEYPAD_NUMBER_OF_COLS = 4; 
+const KEYPAD_NUMBER_OF_ROWS = 4;
+
+const keypadContainer = document.querySelector(".keypad-container");
+generateCalculator();
+
+function generateCalculator() {
+    for (let x = 0; x < KEYPAD_NUMBER_OF_COLS; x++) {
+        let keypadCol = document.createElement("div");
+        keypadCol.classList.add("keypad-col");
+        keypadContainer.append(keypadCol);
+
+        for (let y = 0; y < KEYPAD_NUMBER_OF_ROWS; y++) {
+            let key = document.createElement("div");
+            key.classList.add("key");
+            keypadCol.append(key);
+        }
+    }
+}
+
+
 function add(a,b) {
     return a + b;
 }
